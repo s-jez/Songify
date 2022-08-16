cff525e876abbac7f0f058ce430dbc67a368866c
s-jez <stanislawjez7@gmail.com>
2022-08-16 15:20:51 +0200

    Add next links to Sidebar, Chakra UI components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,10 @@
 import NextImage from "next/image";
+import NextLink from "next/link";
 import {
   Box,
   List,
   ListItem,
   ListIcon,
-  Divider,
-  Center,
   LinkBox,
   LinkOverlay,
 } from "@chakra-ui/layout";
@@ -17,6 +16,33 @@ import {
   MdFavorite,
 } from "react-icons/md";
 
+const navbarElements = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Libary",
+    icon: MdLibraryMusic,
+    route: "/libary",
+  },
+  {
+    name: "Playlist",
+    icon: MdPlaylistAdd,
+    route: "/playlist",
+  },
+  {
+    name: "Favorite",
+    icon: MdFavorite,
+    route: "/favorite",
+  },
+];
 const Sidebar = () => {
   return (
     <Box
@@ -29,6 +55,22 @@ const Sidebar = () => {
       <Box paddingY="20px">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/spotify_icon.svg" width={30} height={30} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={3}>
+            {navbarElements.map((el) => (
+              <ListItem paddingX="18px" fontSize="16px" key={el.name}>
+                <LinkBox>
+                  <NextLink href={el.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon as={el.icon} color="white" marginRight="18px" />
+                      {el.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
