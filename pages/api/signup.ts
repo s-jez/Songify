b421387eb98f61ac369724f24eb69e2ef163ebb1
s-jez <stanislawjez7@gmail.com>
2022-08-23 00:04:20 +0200

    [main]: add signup.ts

---
+++ pages/api/signup.ts
@@ -0,0 +1,38 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync()
+  const {name, email, password, first_name, last_name, age, country} = req.body
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: bcrypt.hashSync(password, salt),
+        first_name,
+        last_name,
+        age,
+        country
+      }
+    })
+  } catch(e) {
+    res.status(401)
+    res.json({error: 'User already exists!'})
+    return
+  }
+  const token = jwt.sign({
+    email: user.email,
+    id: user.id,
+    time: Date.now(),
+  }, 'hello', {expiresIn: '12h'})
+}
