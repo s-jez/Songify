b1327b4671a5142508786b982bc971bbb68fe274
s-jez <stanislawjez7@gmail.com>
2022-08-27 13:31:54 +0200

    [backend]: fix middleware function, to root folder

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,14 @@ const signedinPages = ['/', '/playlist', '/libary']
 export default function middleware(
   request: NextRequest,
 ){
-  if(signedinPages.find((p) => p === request.nextUrl.pathname)) {
+  if(signedinPages.find((url) => url === request.nextUrl.pathname)) {
     const token = request.cookies['TOKEN_SECRET']
     if(!token) {
-      return NextResponse.redirect('/signin')
+      const url = request.nextUrl.clone();
+      console.log(url)
+      url.pathname = "/signin"
+      url.search = "";
+      return NextResponse.redirect(url)
     }
   }
 }
